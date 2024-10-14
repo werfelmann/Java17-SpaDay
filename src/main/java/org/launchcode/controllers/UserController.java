ce58051b1f01f890b47ebb4f85b70bafc7b56f18
werfelmann <werfelmann@gmail.com>
2024-10-14 15:33:38 -0500

    Ch. 14 Studio nearly complete

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,41 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Objects;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("/add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("/add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+    if (Objects.equals(verify, user.getPassword())) {
+        return "redirect:/user/index";
+    } else {
+        model.addAttribute("errorMessage", "Passwords do not match. Please try again");
+        return "user/add";
+        }
+    }
+
+    @GetMapping("/index")
+    public String displayUserIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/serviceSelection")
+    public String displayServiceSelection() {
+        return "serviceSelection";
+    }
+}
---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,46 @@
+package org.launchcode.models;
+
+public class User {
+
+    private int userId;
+    private static int nextUserId = 1;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.userId = nextUserId;
+        nextUserId++;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
